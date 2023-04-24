c4ca3808aff11e3d4e50eaf9e1edf538b9b188eb
carmencara <carmencgald@gmail.com>
2023-04-24 19:10:22 +0200

    .

---
+++ schrodinger.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+#include <cmath>
+#include <fstream>     // Para trabajar con ficheros
+
+using namespace std;
+
+// FUNCIONES QUE SE VAN A UTILIZAR
+
+
+/*---------------------------------------------------------------------
+|                           FUNCIÓN PRINCIPAL                         |
+---------------------------------------------------------------------*/
+int main()
+{
+
+
+    return 0;
+}
