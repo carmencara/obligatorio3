25aa58b9e63be299d2cee24140d3b054d8458a7c
carmencara <carmencgald@gmail.com>
2023-04-24 19:59:05 +0200

    .

--- schrodinger.cpp
+++ schrodinger.cpp
@@ -1,8 +1,18 @@
-
+//---------------------------------------------------------------------
+//                                                                    |
+//          ALGORITMO PARA RESOLVER LA ECUACIÓN DE SCHRÖDINGER        |
+//                              Objetivos:                            |
+//                                                                    |
+//   1. Resolver la ecuación de Schrödinger unidimensional para un    |
+//      potencial cuadrado.                                           |
+//   2. Comprobar que se conserva la norma.                           | 
+//                                                                    |
+//---------------------------------------------------------------------
 
 #include <iostream>
 #include <cmath>
 #include <fstream>     // Para trabajar con ficheros
+#include <ccomplex>    // Para trabajar con números complejos
 
 using namespace std;
 
@@ -14,7 +24,11 @@ using namespace std;
 ---------------------------------------------------------------------*/
 int main()
 {
+    // ------------------ DECLARACIÓN DE VARIABLES --------------------
+    int lambda;
+    int ciclos;
 
+    // ------------------------ INICIALIZACIÓN ------------------------
 
     return 0;
 }
